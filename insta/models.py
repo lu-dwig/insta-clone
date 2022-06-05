82f35559a4611c672a3101aea69c81b42e9446f2
ludwig <murimiludwig@gmail.com>
2022-06-05 23:11:45 +0300

    creation of the post list html

--- insta/models.py
+++ insta/models.py
@@ -9,3 +9,7 @@ class Post(models.Model):
     image = models.ImageField()
     caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    
+    
+    def __str__(self):
+        return self.caption
---
+++ insta/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import (
+    PostListView
+)
+
+app_name = 'insta'
+
+urlpatterns =[
+    path('',PostListView.as_view(), name='post_list'),
+]
